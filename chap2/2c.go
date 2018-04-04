a30d9caceecf7afede8b544aea8776621db03934
Aaron Clements <c.aaron.clements@me.com>
2018-04-04 19:22:49 -0400

    Update 2c.go

--- chap2/2c.go
+++ chap2/2c.go
@@ -2,10 +2,14 @@ package main
 import "fmt"
 
 // Aaron Clements did this.
+//
+// Expected output:     12
+//                      101
+//                      Hello, World
+//
 
 func main() {
 	fmt.Println(len("Hello, World"))
 	fmt.Println("Hello, World"[1])
 	fmt.Println("Hello, " + "World")
 }
-
