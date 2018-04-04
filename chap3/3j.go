e833e94ad15c610d5ce41f254a078c01528fc941
Aaron Clements <aaron@iheart.com>
2018-04-04 19:45:52 -0400

    more go

---
+++ chap3/3j.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+// Aaron Clements did this.
+//
+// Expected output:   My dog's name is Titus. 
+//
+
+func main() {
+	x:= "Titus." 
+	fmt.Println( "My dog's name is", x )
+}
