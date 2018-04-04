3ebc75ea5cc0f531a7607662f282fe050a560041
Aaron Clements <aaron@iheart.com>
2018-04-04 19:37:59 -0400

    more go

---
+++ chap3/3g.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+// Aaron Clements did this.
+//
+// Expected output:     false
+//
+
+func main() {
+	x:= "yo"
+	y:= "hookah"
+	fmt.Println( x == y )
+}
---
+++ chap3/3h.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+// Aaron Clements did this.
+//
+// Expected output:     true
+//
+
+func main() {
+	x:= "yo"
+	y:= "yo"
+	fmt.Println( x == y )
+}
