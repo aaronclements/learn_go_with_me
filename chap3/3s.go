bf3f8f8cfc1c7f4ef2a5f19bb6da38b5dd3c2e8a
Aaron Clements <aaron@iheart.com>
2018-04-04 20:33:34 -0400

    /more

--- chap3/3s.go
+++ chap3/3s.go
@@ -3,12 +3,12 @@ import "fmt"
 
 // Aaron Clements did this.
 //
-// Expected output:  6
-//                   60 
+// Expected output:  Enter degrees in Farenheit: 85
+//                   That is 29.444444444444446 degrees in Celcius.
 //
 
 func main() {
-	fmt.Print("Enter degrees Farenheit: ")
+	fmt.Print("Enter degrees in Farenheit: ")
 	var input float64
 	fmt.Scanf("%f", &input)
 
@@ -16,5 +16,5 @@ func main() {
 	output1 := 5.0 / 9.0
 	output2 := output * output1
 
-	fmt.Println( output2, "degrees Celcius." )
+	fmt.Println( "That is", output2, "degrees in Celcius." )
 }
