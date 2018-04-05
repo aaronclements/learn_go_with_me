ad522a7cf705fb2051dfba7323130520efbd2152
Aaron Clements <aaron@iheart.com>
2018-04-04 20:52:53 -0400

    more go

--- chap4/4c.go
+++ chap4/4c.go
@@ -7,12 +7,35 @@ import "fmt"
 //                   2
 //                   3
 //                   4
-//
+//                   5
+//                   6
+//                   7
+//                   8
+//                   9
+//                   10
 
 func main() {
 	i := 1
-	for i <= 4 {
+	for i <= 10 {
 		fmt.Println(i)
 		i = i + 1
 	}
 }
+
+// 
+// Create a variable named i with the value 1.
+// Is i <= 10? Yes.
+// Print i.
+//
+// Set i to i + 1 (i now equals 2).
+// Is i <= 10? Yes.
+// Print i.
+//
+// Set i to i + 1 (i now equals 3).
+//
+// ...
+//
+// Set i to i + 1 (i now equals 11).
+// Is i <= 10? No.
+// Nothing left to do, so exit.”
+//
