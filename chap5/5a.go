621b59baddccadbda22a39ba710e3aabb4eb0915
Aaron Clements <aaron@iheart.com>
2018-04-10 18:27:35 -0400

    more go

--- chap5/5a.go
+++ chap5/5a.go
@@ -3,11 +3,16 @@ import "fmt"
 
 // Aaron Clements did this.
 //
-//  Expected output:	[0 0 0 0 100]
+//  Expected output:	[0 0 10 0 100 0 0]
 //
+//  Comment:			in an array of 7 x's
+//							the fifth x is 100
+//							the third x is 10
+//							everything else is 0
 
 func main() {
-    var x [5]int
-    x[4] = 100
-    fmt.Println(x)
+    var x [7]int
+	x[4] = 100
+	x[2] = 10
+	fmt.Println(x)
 }
