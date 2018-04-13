3f09a8c36a3308be5706a8f6fc133382c86dfc11
Aaron Clements <c.aaron.clements@me.com>
2018-04-13 13:07:02 -0400

    more go

--- chap6/6a.go
+++ chap6/6a.go
@@ -5,10 +5,10 @@ import "fmt"
 //
 //  Expected output:	46.2
 //
-//				We're creating "xs", which contains 22,33,77,44,55.
-//				"total" is set to 0.0, making it a float
-//				"v" starts at 0.0 and "total" takes that and adds each value in array "xs"
-//				"total" is then divided by the length of the array, giving us an average.
+//		We're creating "xs", which contains 22,33,77,44,55.
+//		"total" is set to 0.0, making it a float
+//		"v" starts at 0.0 and "total" takes that and adds each value in array "xs"
+//		"total" is then divided by the length of the array, giving us an average.
 
 func main() {
 	xs := []float64{22,33,77,44,55}
