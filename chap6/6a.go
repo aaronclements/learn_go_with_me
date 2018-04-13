64f746915c4526d2511def6d1405cccb17871fc5
Aaron Clements <c.aaron.clements@me.com>
2018-04-13 13:08:25 -0400

    more go

--- chap6/6a.go
+++ chap6/6a.go
@@ -6,7 +6,7 @@ import "fmt"
 //  Expected output:	46.2
 //
 //		We're creating "xs", which contains 22,33,77,44,55.
-//		"total" is set to 0.0, making it a float
+//		"total" is set to 0.0, making it a float....
 //		"v" starts at 0.0 and "total" takes that and adds each value in array "xs"
 //		"total" is then divided by the length of the array, giving us an average.
 
