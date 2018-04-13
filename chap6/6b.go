bd340f03fd15e30b23e84a8802c3c1fb84cca11c
Aaron Clements <c.aaron.clements@me.com>
2018-04-13 13:19:07 -0400

    more go

---
+++ chap6/6b.go
@@ -0,0 +1,24 @@
+package main
+import "fmt"
+
+// Aaron Clements did this.
+//
+//  Expected output:	46.2
+//
+//		Same idea.  "averagetime" takes a slice of float64s and
+// 		returns one float64 after dividing by the length of the array.
+//		We get the return value from "averagetime" and use it in "main".
+//
+
+func averagetime(xs []float64) float64 {
+	total := 0.0
+	for _, v := range xs {
+		total +=v
+	}
+	return total / float64(len(xs))
+}
+
+func main() {
+	xs := []float64{22,33,77,44,55}
+	fmt.Println(averagetime(xs))
+}
